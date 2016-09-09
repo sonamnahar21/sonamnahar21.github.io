02f2f752950f992ae88ac62da1af80a512e88cfd
sonamnahar21 <sonamnahar21@gmail.com>
2016-09-08 17:08:26 -0700

    added in

--- js/MainJScript.js
+++ js/MainJScript.js
@@ -15,5 +15,42 @@
           $('#my-navbar').css('background-color', 'transparent');
        }
    });
-});
+   var offsets = document.getElementById('menuContainer').getBoundingClientRect();
 
+        // Get position from top of browser
+                var topoffsets = offsets.top;
+
+                // Binding fuction to windows scroll event
+                $(window).bind('scroll', function () {
+                    if ($(window).scrollTop() > topoffsets) {
+                                $('#menuContainer').addClass('sticky');
+                                $('#menuContainer').addClass('navbar-fixed-top');
+                                document.getElementById('divCompititionContentID').style.paddingTop ='0px';
+                                $('.divletsGo').addClass('menu-padding');
+                                document.getElementById('aboutUSTarget').style.paddingTop='0px';
+                                document.getElementById('aboutUSTarget').style.paddingLeft='70px';
+                            } else {
+                               $('#menuContainer').removeClass('sticky');
+                                $('#menuContainer').removeClass('navbar-fixed-top');
+                                document.getElementById('divCompititionContentID').style.paddingTop ='380px';
+                                //document.getElementById('divCompititionContentID').style.padding='380px';
+                                //document.getElementById("divCompititionContentID").style.textAlign = "center";
+                                $('.divletsGo').removeClass('menu-padding');
+                               document.getElementById('aboutUSTarget').style.paddingTop='20px';
+                               document.getElementById('aboutUSTarget').style.paddingLeft='70px';
+                            }
+                });
+});
+   function openModal(){
+       document.getElementById("bg-repeatID").style.display = "none";
+}   
+function closeModal(){
+       document.getElementById("bg-repeatID").style.display = "block";
+}   
+$('html').click(function (e) {
+    if (e.target.id == 'modal') {
+        alert("hi");
+    } else {
+       document.getElementById("bg-repeatID").style.display = "block";
+    }
+});
